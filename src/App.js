fdd2fa9ee342129425d018fbd0ac550885008c1f
shan-khaitan <shan210706@gmail.com>
2024-09-14 16:25:33 +0530

    Step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>To-Do List</h1>
+   const[task, setTask]=useState('');
+   const[tasks, setTasks]= useState([]);
+
+   const handleAddTask = ()=> {
+    if(task){
+      setTasks([...tasks, task]);
+      setTask('')
+    }
+   }
+   return(
+    <div style={{textAlign: 'center',marginTop: '50px'}}>
+      <h1>To-do List</h1>
+      <input
+      type="text"
+      value={task}
+      onChange={(e) => setTask(e.target.value)}
+      placeholder="Enter a task"
+      />
+      <button onClick={handleAddTask}>Add Task</button>
     </div>
-  );
+   );
 }
 
 export default App;
